c6696467bd2da99b94723ea64ce3b0811f5ac25c
LiamTheNinja5 <liam@posthastedesign.com>
2023-01-30 12:00:39 -0500

    coins assigment

---
+++ Coins/coin.js
@@ -0,0 +1,10 @@
+// set vars
+var penny = 1;
+var nickel = 5;
+var dime = 10;
+var quarter = 25;
+
+document.getElementById("c1").innerHTML = quarter + quarter + quarter + quarter + dime + penny + penny  
+document.getElementById("c2").innerHTML = quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny + penny + penny + penny + penny + penny
+document.getElementById("c3").innerHTML = quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny
+
