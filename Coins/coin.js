59c9968e263ea002632f9ea578f4451115778c95
LiamTheNinja5 <liam@posthastedesign.com>
2023-01-31 14:14:40 -0500

    fixed coin (pls help me)

--- Coins/coin.js
+++ Coins/coin.js
@@ -4,7 +4,7 @@ var nickel = 5;
 var dime = 10;
 var quarter = 25;
 
-document.getElementById("c1").innerHTML = quarter + quarter + quarter + quarter + dime + penny + penny  
-document.getElementById("c2").innerHTML = quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny + penny + penny + penny + penny + penny
-document.getElementById("c3").innerHTML = quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny
+document.getElementById("c1").innerHTML = "quarter + quarter + quarter + quarter + dime + penny + penny + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + penny + penny + penny + penny + penny = " + (quarter + quarter + quarter + quarter + dime + penny + penny + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + nickel + penny + penny + penny + penny + penny)
+document.getElementById("c2").innerHTML = "quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny + penny + penny + penny + penny + penny = " + (quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + penny + penny + penny + penny + penny + penny + penny)
+document.getElementById("c3").innerHTML = "quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + nickel + penny + penny = " + (quarter + quarter + quarter + quarter + dime + dime + dime + dime + dime + dime + dime + dime + nickel + penny + penny)
 
