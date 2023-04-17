c9e257f42a196a724b97a25aba3e68b1aa252821
LiamTheNinja5 <liam@posthastedesign.com>
2023-04-17 14:31:38 -0400

    fix

--- design/design.js
+++ design/design.js
@@ -1,6 +1,7 @@
 let color = 0;
 let ody = 0;
 function dark(){
+    document.getElementById('s1').innerHTML = 'Dark';
     color = 1;
     if (ody == 0){
         document.getElementById('main').className = 'dark';
@@ -11,6 +12,7 @@ function dark(){
 }
 
 function light(){
+    document.getElementById('s1').innerHTML = 'Light';
     color = 0;
     if (ody == 0){
         document.getElementById('main').className = 'light';
@@ -20,10 +22,34 @@ function light(){
 }
 
 function opendyslexic(){
-    if (color == 1){
-        document.getElementById('main').className = 'dark opendyslexic';
-    }else{
-        document.getElementById('main').className = 'light opendyslexic';
+    if (ody == 1){
+        document.getElementById('s2').innerHTML = 'Off';
+        document.getElementById('content').className = '';
+        ody = 0;
+    } else {
+        document.getElementById('s2').innerHTML = 'On';
+        document.getElementById('content').className = 'opendyslexic';
+        ody = 1;
     }
+}
 
+
+function hideset(){
+    document.getElementById('settings').style.display = "none"
+    document.getElementById('showset').style.display = "inline"
+}
+function showset(){
+    document.getElementById('showset').style.display = "none"
+    console.log('gtbakigbekibg')
+    document.getElementById('settings').style.display = "inline"
+}
+
+
+
+
+
+
+function debug(){
+    console.log('ody: '+ody)
+    console.log('color: '+color)
 }
